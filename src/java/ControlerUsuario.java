9e59d99331177b330bd999e9f4133836b47797e2
https://bitbucket.org/uostonsantos/= <uostonsi@gmail.com>
2016-05-31 22:59:12 -0300

    sitação do Aluno, a situação do aluno já pode ser editada, ao inserir um Aluno novo como padrão vem 'Desbloqueado'. Esso ao inserir saldo, forma de implementar a procedure não funciona.

--- src/java/ControlerUsuario.java
+++ src/java/ControlerUsuario.java
@@ -26,6 +26,11 @@ import javax.servlet.http.HttpSession;
 public class ControlerUsuario extends HttpServlet {
 
 
+    
+    
+    
+    
+    //teste
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
